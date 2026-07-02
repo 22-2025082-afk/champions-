fb3eb052a2bbcd513e17c2f8e15d064dd725ef97
22-2025082-afk <22-2025082@edu.city.izumi.osaka.jp>
2026-07-02 13:54:02 +0900

    Update loader.js

--- js/loader.js
+++ js/loader.js
@@ -1,1 +1,14 @@
+// ====================================
+// Pokémon Champions Damage Calculator
 // loader.js
+// Ver.0.4.0
+// ====================================
+
+let pokemonData = [];
+
+async function loadPokemonData() {
+    const response = await fetch("data/pokemon.json");
+    pokemonData = await response.json();
+
+    console.log("ポケモンデータ読み込み完了", pokemonData);
+}
